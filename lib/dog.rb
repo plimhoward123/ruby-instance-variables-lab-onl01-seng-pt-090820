6e0f87eb0f5da336efc5f4a15d09b74a56f34aac
Plimon Howard V <plimon.howard@gmail.com>
2020-10-01 21:27:50 +0000

    Automatically backed up by Learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,13 +1,11 @@
 class Dog
-
-def name=(dog)
-  this_dogs_name = dog
-end
-
-def name
-  this_dogs_name
-end
-
+  def name=(dog_name)
+    this_dogs_name = dog_name
+  end
+ 
+  def name
+    this_dogs_name
+  end
 end
 
 lassie = Dog.new
